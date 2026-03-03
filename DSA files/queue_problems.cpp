7cacaa3aac46d933c680eacede257a9b9d0c1d7b
DhrubTerror <dhrubmahajan17@gmail.com>
2026-03-04 00:57:02 +0530

    K queue in an Array

---
+++ DSA files/queue_problems.cpp
@@ -0,0 +1,97 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+class kQueue{
+    public:
+    int n , k , freeSpot;
+    int *arr , *front , *next , *rear;
+
+    kQueue(int _n , int _k):n(_n), k(_k), freeSpot(0){
+        arr = new int[n];
+        front = new int[k];
+        next = new int[n];
+        rear = new int[k];
+
+        for(int i=0;i<k;i++){
+            front[i] = rear[i] = -1;
+        }
+
+        for(int i=0;i<n;i++){
+            next[i] = i+1;
+        }
+        next[n-1] = -1;
+    }
+
+    //push x element to qith queue
+    bool push(int x, int qi){
+        //check overFlow
+        if(freeSpot == -1){
+            return false;
+        }
+
+        //find first free index
+        int index = freeSpot;
+
+        //update freeSpot
+        freeSpot = next[index];
+    
+        //if it is first element of qith
+        if(front[qi] == -1){
+            front[qi] = index;
+        }
+        else{
+            //else .. if qith already have element then link new element to the q's rearest element
+            next[rear[qi]] = index;
+        }
+
+        //update next 
+        next[index] = -1;
+
+        //update rear
+        rear[qi] = index;
+        arr[index] = x;
+
+        return true;
+    }
+
+
+    // pop element from qith queue
+    int pop(int qi){
+        //underflow check
+        if(front[qi] == -1){
+            return 0;
+        }
+
+        //find index to pop
+        int index = front[qi];
+
+        // front ko update kro
+        front[qi] = next[index];
+
+        //updates freeSpot
+        next[index] = freeSpot;
+        freeSpot = index;
+        
+        return arr[index];
+    }
+
+    ~kQueue(){
+        delete[] arr;
+        delete[] front;
+        delete[] rear;
+        delete[] next;      
+    }
+};
+
+int main(){
+    kQueue kq(8,3);
+
+    cout << kq.push(1,0) << endl;
+    cout << kq.push(2,0) << endl;
+    cout << kq.push(5,1) << endl;
+    cout << kq.push(3,0) << endl;
+
+    cout << kq.pop(1) << endl;
+    cout << kq.pop(0) << endl;
+}
